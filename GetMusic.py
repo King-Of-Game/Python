3ea40d2450109a882a1f81f09220980ddeff2255
King-Of-Game <43782658+King-Of-Game@users.noreply.github.com>
2019-02-16 15:45:03 +0800

    Update GetMusic.py

--- GetMusic.py
+++ GetMusic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# 这串代码把url换成指向.mp3格式或者其他格式的音频即可爬取音乐，别忘了把创建的文件改成xxx.mp3。
 import requests
 
 url = 'http://1251883823.vod2.myqcloud.com/9dae6688vodgzp1251883823/eac880d05285890783036781533/18NS44RnJlwA.mp4'
