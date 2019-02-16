20795187feb9785f74cab50e14f87dd05ec9e558
King-Of-Game <43782658+King-Of-Game@users.noreply.github.com>
2019-02-16 17:07:14 +0800

    Python网站
    这是基于廖雪峰大大的Python实战篇建立的Python网站

---
+++ PythonWeb/www/config_default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 本地的连接配置
+__author__ = 'David Yang'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
